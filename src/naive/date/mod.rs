8fa58a658af1fad2158710bcf3159f5015dad573
NoahSprenger <noah.r.sprenger@gmail.com>
2024-10-05 16:42:17 -0400

    idk

--- src/naive/date/mod.rs
+++ src/naive/date/mod.rs
@@ -99,7 +99,6 @@ mod tests;
     archive_attr(derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash))
 )]
 #[cfg_attr(feature = "rkyv-validation", archive(check_bytes))]
-#[cfg_attr(all(feature = "arbitrary", feature = "std"), derive(proptest_derive::Arbitrary))]
 pub struct NaiveDate {
     yof: NonZeroI32, // (year << 13) | of
 }
